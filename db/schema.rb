e3282df92a94845616d0c7104cd2f3a51f092d98
Сергей Спасский <106836823+dezss01@users.noreply.github.com>
2023-09-10 13:46:50 +0300

    WiP

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_09_184139) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_09_185038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
